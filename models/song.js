454bec3f8e6338b8ff9d318d57d6fe29b49720aa
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-29 10:45:34 -0500

    Changed the song modle to have avg ratings

--- models/song.js
+++ models/song.js
@@ -1,11 +1,18 @@
-const {Schema} = require('mongoose')
+const {Schema, model} = require('mongoose')
 
 const Songs = new Schema(
     {
         title: { type: String, required: true},
         type: { type: String, required: true},
         description: { type: String, required: true},
-        ratings: { type: String, required: true}
+        ratings: [{ type: Number}]
     }
 )
-module.exports = Songs
+
+Songs.virtual('avgRatings').get(function() {
+    const total = this.ratings.reduce((acc,rating) => acc + rating ,0)
+    const count = this.ratings.length
+    return total / count 
+})
+
+module.exports = model('songs',Songs)
