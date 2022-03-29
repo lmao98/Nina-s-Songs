ea6861a8593fddf1c937d979a44df7537ad9c341
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-29 10:27:18 -0500

    steup the basic server logic

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const db = require('./db')
+const routes = require('./routes')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use('/', routes)
+
+db.once('connection',() => {
+    app.listen(PORT,() => {
+        console.log('App listening')
+    })
+})
