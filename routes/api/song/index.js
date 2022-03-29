8fda5b98228d3e1f5c798a0c7e484ae70979fad2
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-29 14:00:05 -0500

    checking if pushed to heroku

--- routes/api/song/index.js
+++ routes/api/song/index.js
@@ -3,4 +3,19 @@ const songModel = require('../../../models/song')
 
 const router = express.Router()
 
+// Add a song
+router.post('/', async(req, res) => {
+    try {
+        const songInfo = req.body
+        const newSong = await songModel.create(songInfo)
+        res.json(newSong) 
+    } catch (error) {
+        console.log(error)
+       res.status(500).json({
+           error: error.message
+       }) 
+    }
+
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.use(express.static(`${__dirname}/client/build`))
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
 app.use('/', routes)
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+   })
 
 db.once('open',() => {
     app.listen(PORT,() => {
