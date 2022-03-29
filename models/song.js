338af93accad37c6e2c4b573fc0497003f663444
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-29 08:42:16 -0500

    Installed mongose and created db models and seed files

---
+++ models/song.js
@@ -0,0 +1,10 @@
+const {Schema} = require('mongoose')
+
+const Songs = new Schema(
+    {
+        title: { type: String, required: true},
+        type: { type: String, required: true},
+        description: { type: String, required: true},
+        ratings: { type: String, required: true}
+    }
+)
