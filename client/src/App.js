8fd402e068ac7b8c6811887c5c1e2ded136a48fc
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-30 11:48:18 -0500

    added skylton for nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Songs from "./components/pages/Songs";
+
 function App() {
   return (
     <BrowserRouter >
-    <div className="row container" >
-      <div className="column bottom">
-        <img src="/nina-left.webp"/>
+      <div className="row container" >
+        <div className="column bottom">
+          <img src="/nina-left.webp" />
+        </div>
+        <div className="column grow">
+          <Header />
+          <Routes >
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/songs" element={<Songs />} />
+          </Routes>
+        </div>
+        <div className="column top">
+          <img src="/nina-right.webp" />
+        </div>
       </div>
-      <div className="column grow">
-        <Header />
-        <Routes >
-          
-        </Routes>
-      </div>
-      <div className="column top">
-        <img src="/nina-right.webp"/>
-      </div>
-    </div>
     </BrowserRouter>
   );
 }
