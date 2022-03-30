490d962613337580f70c1d2dc7cc7ec22a006669
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-30 12:21:04 -0500

    integrated api req with song component

--- client/src/components/pages/Songs.jsx
+++ client/src/components/pages/Songs.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Songs() {
+  const [songList, setSongList] = useState([])
+  useEffect(() => {
+    const updateSongList = async () => {
+      const response = await axios.get("/api/song")
+      setSongList(response.data)
+    }
+    updateSongList()
+  }, [])
+  useEffect(() => {
+    console.log(songList)
+  }, [songList])
+
   return (
     <div>Songs</div>
   )
