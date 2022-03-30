8ba42de29ef8adf877c389b0d82fa4e74fde0457
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-30 10:29:30 -0500

    created basic style structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 
 function App() {
   return (
-    <div >
-
+    <div className="row container" >
+      <div className="column bottom">
+        <img src="/nina-left.webp"/>
+      </div>
+      <div className="column grow"></div>
+      <div className="column top">
+        <img src="/nina-right.webp"/>
+      </div>
     </div>
   );
 }
