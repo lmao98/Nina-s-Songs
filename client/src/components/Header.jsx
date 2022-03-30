5e36ab9f9fdc04d79812fd7193d3667dbd14cff2
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-30 11:39:10 -0500

    added title to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
-    <nav className='row center'>
+    <>
+      <h1 className='row center'>Nina</h1>
+      <nav className='row center'>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/songs">Songs</Link>
-    </nav>
+      </nav>
+    </>
   )
 }
 
