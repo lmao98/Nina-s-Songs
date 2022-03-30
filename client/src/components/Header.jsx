3dbc21658e13c3cb88a9519a2fb0d1fcf4074509
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-30 11:31:48 -0500

    styled the nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,11 +3,11 @@ import {Link} from "react-router-dom"
 
 function Header() {
   return (
-    <div className='row'>
+    <nav className='row center'>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/songs">Songs</Link>
-    </div>
+    </nav>
   )
 }
 
