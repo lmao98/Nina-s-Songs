65e0605990820418353519b83ac561932db0b819
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-31 11:31:47 -0500

    added a render for each review

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Review({ review }) {
   return (
-    <div>{review.description}</div>
+    <div>{(new Array(5)).fill(0).map((e, i) => i < review.value ? '★' : '☆')} '{review.description}' - {review.reviewer}</div>
   )
 }
 
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -4,6 +4,7 @@ import Review from './Review'
 function Reviews({ reviews }) {
   return (
     <div>
+      <h2>Reviews</h2>
       {reviews.map((review) => {
         return <Review review={review} key={review._id} />
       })}
